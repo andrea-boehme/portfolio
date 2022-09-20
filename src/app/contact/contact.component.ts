69e518b552f44929ae4b9ae9dd75170c56dac22b
andrea-boehme <andrea.klayn@gmail.com>
2022-09-20 13:36:26 -0300

    update contact

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit} from '@angular/core';
 import { NgForm } from '@angular/forms';
 
@@ -12,86 +11,30 @@ export class ContactComponent implements OnInit {
   name: string | undefined;
   email: string | undefined;
   message: string | undefined;
-  endpoint = "https://andrea-boehme.developerakademie.net/portfolio/send_mail.php";
-  submitReport = '';
-  submitComplete = false;
 
-
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
 
   ngOnInit(): void {
   }
 
   /**
-   * send the form when it has been filled in
+   * send form content if valid/complete
    *
-   * @param myForm - form content
+   * @param form - form content
    */
-  onSubmit(myForm: NgForm) {
-    if (myForm.valid) {
-      this.sendForm(myForm);
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      this.name = '';
+      this.email = '';
+      this.message = '';
     }
-    
-  }
-
-
-  /**
-   * send the form content
-   *
-   * @param form - form content
-   */
-  sendForm(form: any) {
-    console.log(form);
-    this.name = '';
-    this.email = '';
-    this.message = '';
-    
-  }
-
-
-  /**
-   * show text on successful send
-   *
-   * @param response - send response
-   * @param form - form content
-   */
-  handleResponse(response: any, form: any) {
-    console.log(response);
-    this.submitReport = "Thank you for your message! I will contact you very soon!";
-    this.submitComplete = true;
-    form.reset();
-  }
-
-
-  /**
-   * show text on unsuccessful send
-   *
-   * @param error - send error
-   * @param form - form content
-   */
-  handleError(error: any, form: any) {
-    console.log(error);
-    this.submitReport = "Error occured while sending your message.. Please contact me sending an email to: andrea.bohme@gmail.com";
-    this.submitComplete = true;
-    form.reset();
-  }
-
-
-  /**
-   * closing the reportBox
-   */
-  closeConfirmation() {
-    this.submitComplete = false;
-    this.submitReport = '';
-
   }
 }
 /*
   name: string | undefined;
   email: string | undefined;
   message: string | undefined;
-
   public contact: FormGroup = new FormGroup({
     name: new FormControl('', [
       Validators.required
@@ -102,15 +45,10 @@ export class ContactComponent implements OnInit {
     message: new FormControl('', [
       Validators.required
     ], []),
-
   });
-
-
   constructor() { }
-
   ngOnInit(): void {
   }
-
   onSubmit() {
       this.name = '';
       this.email = '';
