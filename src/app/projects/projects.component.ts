5543ebfca2e2be2478ae262af3c88abf614e748c
andrea-boehme <andrea.klayn@gmail.com>
2022-09-23 15:02:33 -0300

    update projects

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
@@ -14,7 +15,8 @@ export class ProjectsComponent implements OnInit {
       "url": 'https://andrea-boehme.developerakademie.net/kanban-app/views/login.html',
       "git": 'https://github.com/andrea-boehme/Kanban-app',
       "description": 'Create and manage tasks by dragging and dropping them',
-      "tool": 'HTML, CSS, JavaScript and Backend Integration'
+      "tool": 'HTML, CSS, JavaScript and Backend Integration',
+      "tech": 'JavaScript'
     },
     {
       "title": 'CRM System',
@@ -22,7 +24,8 @@ export class ProjectsComponent implements OnInit {
       "url": 'https://andrea-boehme.developerakademie.net/simple-crm/index.html',
       "git": 'https://github.com/andrea-boehme/simple-crm',
       "description": 'Manage data and analyse statistics with a simple CRM tool',
-      "tool": 'Angular (TypeScript, SCSS) and Firebase'
+      "tool": 'Angular (TypeScript, SCSS) and Firebase',
+      "tech": 'Angular'
     },
     {
       "title": 'Ring of Fire',
@@ -30,7 +33,8 @@ export class ProjectsComponent implements OnInit {
       "url": 'https://andrea-boehme.developerakademie.net/ring-of-fire/index.html',
       "git": 'https://github.com/andrea-boehme/ring-of-fire',
       "description": 'Play this card game with your friends from different devices',
-      "tool": 'Angular and Firebase'
+      "tool": 'Angular and Firebase',
+      "tech": 'Angular'
     },
     {
       "title": 'El Pollo Loco Game',
@@ -38,7 +42,8 @@ export class ProjectsComponent implements OnInit {
       "url": 'https://andrea-boehme.developerakademie.net/el-pollo-loco/index.html',
       "git": 'https://github.com/andrea-boehme/El-Pollo-Loco',
       "description": 'Play this jump and run game built using object-oriented programming',
-      "tool": 'JavaScript (OOP)'
+      "tool": 'JavaScript (OOP)',
+      "tech": 'JavaScript'
     },
     {
       "title": 'Pokedex',
@@ -46,7 +51,8 @@ export class ProjectsComponent implements OnInit {
       "url": 'https://andrea-boehme.developerakademie.net/pokemon/index.html',
       "git": 'https://github.com/andrea-boehme/pokemon',
       "description": 'Load creatures with images and abilities from a rest API.',
-      "tool": 'JavaScript, Bootstrap and API'
+      "tool": 'JavaScript, Bootstrap and API',
+      "tech": 'JavaScript'
     },
     {
       "title": 'Portfolio',
@@ -54,13 +60,36 @@ export class ProjectsComponent implements OnInit {
       "url": 'https://andrea-boehme.developerakademie.net/portfolio/index.html',
       "git": 'https://github.com/andrea-boehme/portfolio',
       "description": 'My own portfolio website',
-      "tool": 'Angular'
+      "tool": 'Angular',
+      "tech": 'Angular'
     }
   ]
+
+
+
+  currentFilter = 'All';
+  selectedProjects = this.projects;
 
   constructor() { }
 
   ngOnInit(): void {
+
+ }
+
+ updateFilter(filter: string) {
+  if (filter === 'All') {
+    this.currentFilter = 'All';
+    this.selectedProjects = this.projects;
+    console.log(this.selectedProjects);
+  } else if (filter == 'Angular') {
+    this.currentFilter = 'Angular';
+    this.selectedProjects = this.projects.filter(project => project.tech == 'Angular');
+    console.log(this.selectedProjects);
+  } else if (filter == 'JavaScript') {
+    this.currentFilter = 'JavaScript';
+    this.selectedProjects = this.projects.filter(project => project.tech == 'JavaScript');
+    console.log(this.selectedProjects);
   }
+}
 
 }
